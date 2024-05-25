52714fc7def40521a3de03cf9f236cb953d4fdf3
dev8 <yuka@MacBook-Air.local>
2024-05-25 15:28:21 +0900

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,6 +7,13 @@ export default function App({ Component, pageProps }) {
     <ChakraProvider>
       <Head>
         <title>Develop8 Studio</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/main_logo.png" />
+        <link rel="apple-touch-icon" href="/main_logo.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content="https://dev8.me/twitter_cards.png" />
+        <meta name="twitter:title" content="Develop8 Studio" />
+        <meta name="twitter:description" content="About me" />
       </Head>
       <Component {...pageProps} />
     </ChakraProvider>
