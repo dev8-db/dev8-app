1e1989e84c5dfaa5e9d945461b3138c5c739bfc9
dev8 <yuka@MacBook-Air.local>
2024-05-26 14:36:07 +0900

    Update Fav

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,8 +9,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Develop8 Studio</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/main_logo.png" />
-        <link rel="apple-touch-icon" href="/main_logo.png" />
+        <link rel="icon" href="/desktop-fav.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content="https://dev8.me/twitter_cards.png" />
         <meta name="twitter:title" content="Develop8 Studio" />
