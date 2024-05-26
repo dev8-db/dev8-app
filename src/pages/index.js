9096abd0df3b23020929d69ba315f4168f57e58c
dev8 <yuka@MacBook-Air.local>
2024-05-27 00:21:16 +0900

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,51 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react';
-import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react';
-import { Heading } from '@chakra-ui/react';
-import { Stack, HStack, VStack, StackDivider } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
+import React from 'react';;
 import {
-  Flex,
-  Avatar,
-  IconButton,
-  Image
+  Image, Text, VStack, Heading, Card, CardHeader, CardFooter, CardBody, Stack, Button
 } from "@chakra-ui/react";
-import { Divider } from '@chakra-ui/react';
-import { LinkBox, LinkOverlay, Tag, SimpleGrid } from '@chakra-ui/react';
-import { useDisclosure } from '@chakra-ui/react';
-import { useBreakpointValue } from '@chakra-ui/react';
-import { Code, Link } from '@chakra-ui/react';
+import { useRouter } from 'next/router';  
 
 export default function IndexPage() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push('https://nook-dev.vercel.app/');
+  };
+
   return (
     <div>
-      <VStack className='mb-5 mr-[10px] ml-[10px] md:mr-5 md:ml-'>
+      <VStack className='mb-10 mr-[10px] ml-[10px] md:mr-5 md:ml-' textAlign="center">
         <Image src="/top.png" width={500} />
         <Text className='text-gray-500'>Combining front-end development with UI design allows you to create</Text>
         <Text className='text-gray-500'>seamless and engaging digital experiences that are both functional and beautiful!</Text>
       </VStack>
-      <div className='mr-[10px] ml-[10px] md:mr-5 md:ml-5'>
-      </div>
+      <VStack className='mr-[10px] ml-[10px] md:mr-5 md:ml-' textAlign="center">
+        <Heading className='mb-5'>Goods</Heading>
+        <Card
+          direction={{ base: 'column', sm: 'row' }}
+          overflow='hidden'
+          variant='outline'
+        >
+          <Image
+            objectFit='cover'
+            maxW={{ base: '100%', sm: '200px' }}
+            src="/trip.svg"
+            padding="5"
+          />
+          <Stack>
+            <CardBody>
+              <Heading size='md'>Nook.dev</Heading>
+              <Text py='2'>
+                Byte-sized Chats, Mega-sized Ideas! Real time chat app!
+              </Text>
+            </CardBody>
+            <CardFooter>
+              <Button variant='solid' colorScheme='blue' onClick={handleClick}>
+                Try to use
+              </Button>
+            </CardFooter>
+          </Stack>
+        </Card>
+      </VStack>
     </div>
   )
 }
