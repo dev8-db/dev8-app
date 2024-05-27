0c331e0ef1eaa1b44bdaa681be8abdc73aafe27f
dev8 <yuka@MacBook-Air.local>
2024-05-28 00:01:16 +0900

    Update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 
 export default function Footer() {
     return (
-        <div className="bg-gray-900 text-gray-300 py-5 mt-20">
+        <div className="bg-gray-900 text-gray-300 py-5 mt-[75px]">
             <div className="container mx-auto px-4 lg:px-0 flex flex-col lg:flex-row justify-between items-center">
                 <Text className="text-sm mb-4 lg:mb-0 lg:mr-8">&copy; 2024 Develop8 Studio. All rights reserved.</Text>
                 <div className="flex flex-wrap lg:flex-nowrap gap-5">
--- src/pages/notices.js
+++ src/pages/notices.js
@@ -1,71 +1,17 @@
 import Head from "next/head";
-import { Box } from '@chakra-ui/react';
 import React from 'react';
 import { Heading } from '@chakra-ui/react';
 import { VStack } from '@chakra-ui/react';
-import { Text } from '@chakra-ui/react';
-import { LinkBox, LinkOverlay } from '@chakra-ui/react';
+import NoticesList from "@/components/Notices";
 
 export default function Notices() {
-    const posts = [
-        {
-            dateTime: '2024-05-25T22:00:00+09:00',
-            heading: 'Updated the site design.',
-            text: 'I significantly changed the site design using Chakra UI.',
-            link: "none"
-        },
-    ];
-
-    const timeDifference = (dateTime) => {
-        const now = new Date();
-        const postDate = new Date(dateTime);
-        const diffInSeconds = Math.floor((now - postDate) / 1000);
-        const years = Math.floor(diffInSeconds / (3600 * 24 * 365));
-        const months = Math.floor(diffInSeconds / (3600 * 24 * 30));
-        const days = Math.floor(diffInSeconds / (3600 * 24));
-        const hours = Math.floor(diffInSeconds / 3600);
-        const minutes = Math.floor(diffInSeconds / 60);
-
-        if (years > 0) return `${years} year${years > 1 ? 's' : ''} ago`;
-        if (months > 0) return `${months} month${months > 1 ? 's' : ''} ago`;
-        if (days > 0) return `${days} day${days > 1 ? 's' : ''} ago`;
-        if (hours > 0) return `${hours} hour${hours > 1 ? 's' : ''} ago`;
-        return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
-    };
-
-    const Post = ({ dateTime, heading, text, link }) => (
-        <LinkBox as='article' maxW='sm' p='5' borderWidth='1px' rounded='md' m='2' boxShadow="sm">
-            <Box as='time' dateTime={dateTime} className='opacity-50'>
-                {timeDifference(dateTime)}
-            </Box>
-            {link !== "none" ? (
-                <Heading size='md' my='2' className="text-gray-700">
-                <LinkOverlay href={link}>
-                    {heading}
-                </LinkOverlay>
-                </Heading>
-            ) : (
-                <Heading size='md' my='2' className="text-gray-700">
-                {heading}
-                </Heading>
-            )}
-            <Text className="text-gray-500">
-            {text}
-            </Text>
-        </LinkBox>
-    );
-
     return (
         <div className="mr-[10px] ml-[10px] md:mr-0 md:ml-0">
             <Head>
                 <title>Latest Notices -Develop8 Studio</title>
             </Head>
             <VStack className='mb-5'><Heading className="text-gray-700">Latest Notices</Heading></VStack>
-            <VStack spacing={5}>
-            {posts.map((post, index) => (
-                <Post key={index} {...post} />
-            ))}
-            </VStack>
+            <NoticesList />
         </div>
     )
 }
