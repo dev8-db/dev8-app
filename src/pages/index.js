1544ff7cf987c5c459b266894fcc31d1a9282d70
dev8 <yuka@MacBook-Air.local>
2024-05-30 00:53:15 +0900

    Update

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,17 +8,18 @@ import Footer from "@/components/Footer";
 export default function IndexPage() {
   return (
     <div>
-      <VStack>
+      {/* <VStack>
         <Flex
-          bgGradient="linear(to-r, orange.400, pink.400)"
+          bgGradient="linear(to-r, blue.300, green.300)"
           className='rounded-lg mb-5 w-[90%] md:w-[75%] h-[125px] md:h-[200px] items-center justify-center'
         />
-      </VStack>
+      </VStack> */}
       <VStack textAlign="center">
-        <Heading>🐱🍡🐶</Heading>
+        {/* <Heading>🐱🍡🐶</Heading> */}
+        <Heading letterSpacing={5}>🧀🥪🍎</Heading>
       </VStack>
       <VStack className='mb-10'>
-        <Heading lineHeight="tall" className='text-gray-700 w-[90%] md:w-[75%]'>Front-end And UI Design</Heading>
+        <Heading lineHeight="tall" fontSize={{base: "50", sm: "100"}} className='text-gray-700 w-[90%] md:w-[75%]'><span className='text-green-500'>Front-end</span> And UI Design</Heading>
         <Text className='text-gray-500 w-[90%] md:w-[75%]'>Front-end and UI design merge creativity with technology, transforming ideas into interactive, user-friendly experiences. It's a field of endless innovation and immediate, visible results!</Text>
       </VStack>
       <VStack className='mb-10' textAlign="right">
