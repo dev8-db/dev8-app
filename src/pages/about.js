983b70f9a638b7bdd855747ff212508a5385a3b0
dev8 <yuka@MacBook-Air.local>
2024-05-29 23:38:56 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,7 +19,9 @@ import {
   Box,
   Link,
   TableCaption,
-  Tfoot
+  Tfoot,
+  Code,
+  Stack
 } from "@chakra-ui/react";
 
 export default function About() {
@@ -29,21 +31,25 @@ export default function About() {
         <title>About me -Develop8 Studio</title>
       </Head>
       <VStack className='mb-5'><Heading className="text-gray-700">About me</Heading></VStack>
-      <VStack className='mb-5'><Image src="/dog.svg" className="rounded-full w-20 h-20" /></VStack>
+      <VStack className='mb-5'>
+        <Image src="/dog.svg" className="rounded-full w-20 h-20" />
+        <Text className="font-bold">Develop8</Text>
+        <Text className="w-[80%] md:w-[50%] text-gray-500" lineHeight="tall">フロントエンドを中心とした開発を行っている中学生です。好きなフレームワークはNext.jsです。趣味は読書と音楽を聴くことです。</Text>
+      </VStack>
       <TableContainer>
         <Table variant='simple'>
           <Tbody>
           <Tr>
               <Td className="text-gray-500">Name</Td>
-              <Td>Develop8</Td>
+              <Td><Code colorScheme='red'>Develop8</Code></Td>
             </Tr>
             <Tr>
               <Td className="text-gray-500">Location</Td>
-              <Td>Hokkaido, Japan</Td>
+              <Td><Code>Hokkaido, Japan</Code></Td>
             </Tr>
             <Tr>
               <Td className="text-gray-500">Things I love</Td>
-              <Td>Front-end, Reading, Listening to music</Td>
+              <Td><Code>Front-end, Reading, Listening to music</Code></Td>
             </Tr>
           </Tbody>
         </Table>
