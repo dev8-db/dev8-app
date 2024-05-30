a8ca951694e588c44038583c6c471e0975b3f827
dev8 <yuka@MacBook-Air.local>
2024-05-30 23:39:11 +0900

    Update FOoter

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <Box bg="gray.100" className="py-5 px-5 mt-[75px]">
         <Flex className="w-[90%] md:max-w-[1200px] mx-auto flex justify-between items-center flex-col md:flex-row text-center md:text-left">
             <VStack spacing={2.5} align={["center", "center", "flex-start"]} className="mb-5 md:mb-0">
-                <Link href="/"><Image src="/develop8-studio.png" alt="Develop8" width="100px" /></Link>
+                <Link href="/"><Image src="/cheese-face.png" width="20" /></Link>
                 <Text className="text-sm text-gray-600">&copy; 2024 Develop8 Studio. All rights reserved.</Text>
             </VStack>
             <Stack direction="row" spacing={[2.5, 5, 10]} align="flex-start" className="flex-col md:flex-row">
